1d1f1d7ee44cb2f9739a9df9b1793d8dacb1aca7
Your New Name <tonhi16022003@gmail.com>
2025-04-02 14:17:25 +0700

    creat dashboard

---
+++ src/page/dashboard/DashboardPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <div className="dashboard-container">
+      <h1 className="dashboard-title">Thống kê kinh doanh</h1>
+      <div className="dashboard-content">
+        {/* Dashboard content will go here */}
+        <p>Dashboard content placeholder</p>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
